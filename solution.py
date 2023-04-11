87f348cc12919d93228cc370f68ad47cd45eeab5
Eugenia <zhexter@gmail.com>
2023-04-11 12:46:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from hyppo.ksample import MMD
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 12162367 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
