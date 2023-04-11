8bb7a4ab1f72ef9cf18871f6210882a29b6d56f6
Eugenia <zhexter@gmail.com>
2023-04-11 14:58:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    #p_value = MMD(compute_kernel="laplacian", gamma=1).test(x, y)[2]
-    p_value = sps.anderson_ksamp([x, y])[1]
+    #p_value = MMD(compute_kernel="laplacian", gamma=1).test(x, y)[1]
+    p_value = sps.anderson_ksamp([x, y])[2]
     alpha = 0.06
     return p_value < alpha # Ваш ответ, True или False
