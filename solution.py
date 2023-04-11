26db9616bc625e898db677b728d786f137ba06e5
Eugenia <zhexter@gmail.com>
2023-04-11 13:43:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from hyppo.ksample import MMD
+import scipy.stats as sps
 
 chat_id = 12162367 # Ваш chat ID, не меняйте название переменной
 
@@ -8,6 +9,7 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    p_value = MMD(compute_kernel="laplacian", gamma=1).test(x, y)[1]
+    #p_value = MMD(compute_kernel="laplacian", gamma=1).test(x, y)[1]
+    p_value = sps.anderson_ksamp([x, y])[1]
     alpha = 0.06
     return p_value < alpha # Ваш ответ, True или False
